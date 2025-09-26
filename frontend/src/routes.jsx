51dd25041f55e07fa29914871deaa0dd510566b7
noname <noname@gmail.com>
2025-09-26 15:29:37 +0530

    to pull from main

--- frontend/src/routes.jsx
+++ frontend/src/routes.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 // Import pages/components (capitalized)
 import Homepage from './pages/homepage';
 import EraSelect from './pages/era_select';
+import CharacterSelect from './pages/character_select';
 
 const AppRoutes = () => {
   return (
@@ -14,6 +15,8 @@ const AppRoutes = () => {
 
         {/* Era selection page */}
         <Route path="/era-select" element={<EraSelect />} />
+        {/* Character selection page */}
+        <Route path="/character-select" element={<CharacterSelect />} />
       </Routes>
     </Router>
   );
